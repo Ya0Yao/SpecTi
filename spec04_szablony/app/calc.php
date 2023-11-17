80d01b0ff04f17a4a7bce28d6e37fbe17c2b0b3e
Ya0Yao <127833137+Ya0Yao@users.noreply.github.com>
2023-11-17 11:12:27 +0100

    Add files via upload

--- spec04_szablony/app/calc.php
+++ spec04_szablony/app/calc.php
@@ -1,6 +1,8 @@
 <?php
 // KONTROLER strony kalkulatora
 require_once dirname(__FILE__).'/../config.php';
+//załaduj Smarty
+require_once _ROOT_PATH.'/lib/smarty/Smarty.class.php';
 
 // W kontrolerze niczego nie wysyła się do klienta.
 // Wysłaniem odpowiedzi zajmie się odpowiedni widok.
@@ -8,69 +10,71 @@ require_once dirname(__FILE__).'/../config.php';
 
 // 1. pobranie parametrów
 
-include _ROOT_PATH.'/app/security/check.php';
 
 
-function getParams(&$amount, &$years ,&$percent, &$role){
-	$amount = isset($_REQUEST['amount']) ? $_REQUEST['amount'] : null;
-	$years = isset($_REQUEST['years']) ? $_REQUEST['years'] : null;
-	$percent = isset($_REQUEST['percent']) ? $_REQUEST['percent'] : null;	
-	$role = $_SESSION['role'];
+function getParams(&$form){
+	$form['amount'] = isset($_REQUEST['amount']) ? $_REQUEST['amount'] : null;
+	$form['years'] = isset($_REQUEST['years']) ? $_REQUEST['years'] : null;
+	$form['percent'] = isset($_REQUEST['percent']) ? $_REQUEST['percent'] : null;	
+	// $role = $_SESSION['role'];
 }
 
-function validate(&$amount, &$years ,&$percent,&$messages, &$role){
+function validate(&$form,&$infos,&$msgs,&$hide_intro){
 
 	// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
 	// sprawdzenie, czy parametry zostały przekazane
-	if ( ! (isset($amount) && isset($years) && isset($percent))) {
+	if ( ! (isset($form['amount']) && isset($form['years']) && isset($form['percent']))) {
 		//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
 		return false;
 	}
+	$hide_intro = true;
 
+	$infos [] = 'Przekazano parametry.';
 	// sprawdzenie, czy potrzebne wartości zostały przekazane
-	if ( $amount == "") {
-		$messages [] = 'Nie podano kwoty';
+	if ( $form['amount'] == "") {
+		$msgs [] = 'Nie podano kwoty';
 	}
-	if ( $years == "") {
-		$messages [] = 'Nie podano liczby lat';
+	if ( $form['years'] == "") {
+		$msgs [] = 'Nie podano liczby lat';
 	}
-	if ( $percent == "") {
-		$messages [] = 'Nie podano procentów';
+	if ( $form['percent'] == "") {
+		$msgs [] = 'Nie podano procentów';
 	}
 
 	//nie ma sensu walidować dalej gdy brak parametrów
-	if (empty( $messages )) {
+	if (empty( $msgs )) {
 		
-		// sprawdzenie, czy $amount i $years są liczbami całkowitymi
-		if (! is_numeric( $amount )) {
-			$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+		// sprawdzenie, czy $form['amount'] i $form['years'] są liczbami całkowitymi
+		if (! is_numeric( $form['amount'] )) {
+			$msgs [] = 'Pierwsza wartość nie jest liczbą całkowitą';
 		}
-		if (! is_numeric( $percent )) {
-			$messages [] = 'Nie jest to liczba float';
+		if (! is_numeric( $form['percent'] )) {
+			$msgs [] = 'Nie jest to liczba float';
 		}
-		$percent = floatval($percent);
-		$amount = intval($amount);
-		if($role == 'user'){
-			if( $amount > 100000){
-				$messages [] = 'Nie masz uprawnień dla tak dużych kwot';
-			}
-			if( $percent < 5){
-				$messages [] = 'Nie masz uprawnień dla tak małego oprocentowania';
-			}
-		}
-		if (count($messages) != 0) return false ;
+		// if($role == 'user'){
+		// 	if( $form['amount'] > 100000){
+		// 		$msgs [] = 'Nie masz uprawnień dla tak dużych kwot';
+		// 	}
+		// 	if( $form['percent'] < 5){
+		// 		$msgs [] = 'Nie masz uprawnień dla tak małego oprocentowania';
+		// 	}
+		// }
+		
+		if (!empty($msgs) ) return false ;
 		else return true;
 	}
 }
 
-function process(&$amount, &$years ,&$percent,&$messages,&$result, &$all){
+function process(&$form,&$infos,&$msgs,&$result, &$all){
 
 	// 3. wykonaj zadanie jeśli wszystko w porządku
-	if (empty ( $messages )) { // gdy brak błędów
+	if (empty ( $msgs )) { // gdy brak błędów
 		
 		//konwersja parametrów na int
-		
-		$percent2 = ($percent + 100 ) * $years;
+		$percent = floatval($form['percent']);
+		$amount = intval($form['amount']);
+
+		$percent2 = ($percent + 100 ) * $form['years'];
 
 		$all = ($amount * $percent2) / 100;
 		$result = $all/12;
@@ -78,24 +82,41 @@ function process(&$amount, &$years ,&$percent,&$messages,&$result, &$all){
 	}
 }
 
-$amount = null;
-$years = null;
-$percent = null;
+$form = null;
+$infos = array();
+$msgs = array();
 $result = null;
-$messages = array();
 $all = null;
-$role = null;
-getParams($amount,$years,$percent,$role);
-if ( validate($amount, $years,$percent, $messages , $role)){
-	process($amount, $years, $percent, $messages, $result,$all);
+// $role = null;
+getParams($form);
+if ( validate($form,$infos,$messages,$hide_intro)){
+	process($form,$infos,$messages,$result,$all);
 }
 // 4. Wywołanie widoku z przekazaniem zmiennych
-// - zainicjowane zmienne ($messages,$x,$years,$percent,$result)
+// - zainicjowane zmienne ($msgs,$x,$form['years'],$form['percent'],$result)
 //   będą dostępne w dołączonym skrypcie
 
-$page_title = 'Przykład 03';
-$page_description = 'Najprostsze szablonowanie oparte na budowaniu widoku poprzez dołączanie kolejnych części HTML zdefiniowanych w różnych plikach .php';
-$page_header = 'Proste szablony';
-$page_footer = 'przykładowa tresć stopki wpisana do szablonu z kontrolera';
+// $page_title = 'Przykład 03';
+// $page_description = 'Najprostsze szablonowanie oparte na budowaniu widoku poprzez dołączanie kolejnych części HTML zdefiniowanych w różnych plikach .php';
+// $page_header = 'Proste szablony';
+// $page_footer = 'przykładowa tresć stopki wpisana do szablonu z kontrolera';
 
-include 'calc_view.php';
+// 4. Przygotowanie danych dla szablonu
+
+$smarty = new Smarty();
+
+$smarty->assign('app_url',_APP_URL);
+$smarty->assign('root_path',_ROOT_PATH);
+$smarty->assign('page_title','Przykład 04');
+$smarty->assign('page_description','Profesjonalne szablonowanie oparte na bibliotece Smarty');
+$smarty->assign('page_header','Szablony Smarty');
+
+//pozostałe zmienne niekoniecznie muszą istnieć, dlatego sprawdzamy aby nie otrzymać ostrzeżenia
+$smarty->assign('form',$form);
+$smarty->assign('result',$result);
+$smarty->assign('msgs',$msgs);
+$smarty->assign('infos',$infos);
+$smarty->assign('all',$all);
+
+// 5. Wywołanie szablonu
+$smarty->display(_ROOT_PATH.'/app/calc.html');
